902a6cc997609cc19f9ca00e615cdf967757bef2
Glitch (jungle-language) <none>
2018-09-14 10:49:55 +0000

    🌥🛩 Checkpoint ./server.js:1320896/132

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ app.get("/", function (req, res) {
 app.get('/api/timestamp/:date_string', (req, res) => {
   //res.send('<h2 style="color: forestgreen;">API Endpoint reached successfully!</h1>');
   // if(res.params.date_string == null) {res.send(new Date())}
+  let query = req.params.date_string;
   res.send(req.params);
   let regex = /\d{4}-\d{2}-\d{2/;
-  
+  console.log(regex.test(query);
   
 });
 
