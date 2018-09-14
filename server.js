819cc558a3e92aa87e97e96d58c11db1876abc1d
Glitch (jungle-language) <none>
2018-09-14 21:00:43 +0000

    🐺🚎 Checkpoint ./server.js:1320896/865

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 var express = require('express');
+// instantiate the express web-app framework
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS so that API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  
 
-// http://expressjs.com/en/starter/static-files.html
+// middleware that allows express to serve static resources.  
+// express works with dynamically generated content by default
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
+  // responds to get requests at root directory with front-end html file.
+  // This is made possible by using express.static middleware
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
 app.get('/api/timestamp', (req,res) => {
+  // handler for empty request
+  let dateString = req.params.date_string;
   let date = new Date();
-  res.json(date);
+  res.json({"unix": date.getTime(dateString), "utc" : date.toUTCString(dateString) });
 
 });
 
 app.get('/api/timestamp/:date_string', (req, res) => {
   //res.send('<h2 style="color: forestgreen;">API Endpoint reached successfully!</h1>');
-
+  
+  // date_string is received from the browser as a string
   let dateString = req.params.date_string;
+  //console.log(typeof req.params.date_string);
   let regex = /\d{4}-?\d{2}-?\d{2}/g;
     
   if(regex.test(dateString)){
@@ -40,9 +48,6 @@ app.get('/api/timestamp/:date_string', (req, res) => {
     
 //     }
     
-    
-    // res.send('<h2 style="color: forestgreen;">query validated</h1>');
-
     res.json({"unix": date.getTime(dateString), "utc" : date.toUTCString(dateString) });
     } else {
   // fails regex test
